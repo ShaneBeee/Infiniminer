83d95a238d8553a0b52a11ac543198050627f72d
ShaneBee <shanebolenback@me.com>
2023-10-16 07:12:55 -0700

    Biome - add some more default biomes to play with

--- Assets/Scripts/World/Biome/Biomes.cs
+++ Assets/Scripts/World/Biome/Biomes.cs
@@ -1,10 +1,14 @@
+using System.Diagnostics.CodeAnalysis;
 using World.Block;
 
 namespace World.Biome {
 
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
     public static class Biomes {
 
         public static readonly Biome DEFAULT = GetDefault();
+        public static readonly Biome TNT = GetTNTBiome();
+        public static readonly Biome STONE = GetStoneBiome();
 
         private static Biome GetDefault() {
             return new Biome("default",
@@ -12,6 +16,38 @@ namespace World.Biome {
                         .SolidGroundHeight(42)
                         .TerrainHeight(12)
                         .TerrainScale(0.25f))
+                .AddOreBlob(new OreBlob("diamond", Blocks.DIAMOND)
+                    .MinHeight(10)
+                    .MaxHeight(20)
+                    .Scale(0.1f)
+                    .Threshold(0.5f)
+                    .NoiseOffset(0f)
+                );
+        }
+
+        private static Biome GetTNTBiome() {
+            return new Biome("tnt",
+                    new BiomeProperties()
+                        .SolidGroundHeight(42)
+                        .TerrainHeight(12)
+                        .TerrainScale(0.25f)
+                        .GroundBlock(Blocks.TNT))
+                .AddOreBlob(new OreBlob("diamond", Blocks.DIAMOND)
+                    .MinHeight(10)
+                    .MaxHeight(20)
+                    .Scale(0.1f)
+                    .Threshold(0.5f)
+                    .NoiseOffset(0f)
+                );
+        }
+        
+        private static Biome GetStoneBiome() {
+            return new Biome("stone",
+                    new BiomeProperties()
+                        .SolidGroundHeight(42)
+                        .TerrainHeight(12)
+                        .TerrainScale(0.25f)
+                        .GroundBlock(Blocks.ROCK))
                 .AddOreBlob(new OreBlob("diamond", Blocks.DIAMOND)
                     .MinHeight(10)
                     .MaxHeight(20)
