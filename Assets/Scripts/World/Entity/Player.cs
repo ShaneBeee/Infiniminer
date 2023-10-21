b19a2666d99fe7837fbae0487be283e38788a739
ShaneBee <shanebolenback@me.com>
2023-10-20 23:32:44 -0700

    Player - more updates for jumping/flying

--- Assets/Scripts/World/Entity/Player.cs
+++ Assets/Scripts/World/Entity/Player.cs
@@ -15,7 +15,7 @@ namespace World.Entity {
         [SerializeField] private Transform highlightBlock;
         [SerializeField] private Transform placeBlock;
         [SerializeField] private Transform playerBody;
-        private new Camera camera;
+        private Camera playerCamera;
         private Rigidbody rigidBody;
         private BoxCollider playerCollider;
 
@@ -30,11 +30,12 @@ namespace World.Entity {
         private bool isFlying;
         private float _mouseHorizontal;
         private float _mouseVertical;
+        private float jumpKeyLastPressed;
 
         private void Start() {
             var cameraObject = gameObject.GetComponentInChildren<Camera>();
             if (cameraObject != null) {
-                this.camera = cameraObject;
+                this.playerCamera = cameraObject;
             } else {
                 Debug.LogError("Camera on player == null");
             }
@@ -56,35 +57,55 @@ namespace World.Entity {
                 PlaceCursorBlock();
             }
 
+            // Double jump quickly to enable/disable flight mode
+            if (Input.GetKeyDown(KeyCode.Space) && gamemode == Gamemode.CREATIVE) {
+                // Max double jump delay for flight mode
+                if (Time.time - jumpKeyLastPressed < 0.35f) {
+                    isFlying = !isFlying;
+                    if (isFlying) {
+                        // reset velocity so we don't fly away
+                        ResetVelocity();
+                    }
+                }
+
+                jumpKeyLastPressed = Time.time;
+            }
         }
 
         private void FixedUpdate() {
             if (IsMenuOpen()) return;
+
+            // Check ability to fly
+            if (gamemode == Gamemode.SURVIVAL) {
+                isFlying = false;
+            }
+
             // Apply gravity
             if (gamemode != Gamemode.SPECTATOR && !isFlying) {
                 var pull = Vector3.down * (gravity * rigidBody.mass);
                 rigidBody.AddForce(pull, ForceMode.Acceleration);
             }
-            
+
             // Check if player is on ground
             var playerBlockPos = transform.position.ToVector3Int();
             var blockBelow = playerBlockPos + Vector3Int.down;
             if (world.GetBlock(blockBelow) != Blocks.AIR &&
                 Math.Abs((transform.position.y) - playerBlockPos.y) < 0.01) {
                 isGrounded = true;
+                isFlying = false;
             } else {
                 isGrounded = false;
             }
 
             // Handle jumping
-            if (isGrounded && Input.GetKey(KeyCode.Space)) {
-                if (gamemode == Gamemode.SPECTATOR || (gamemode == Gamemode.CREATIVE && isFlying)) {
+            if (Input.GetKey(KeyCode.Space)) {
+                if ((gamemode == Gamemode.SURVIVAL || gamemode == Gamemode.CREATIVE) && isGrounded) {
+                    isGrounded = false;
+                    rigidBody.AddForce(Vector3.up * (jumpForce * 100));
+                } else if ((gamemode == Gamemode.CREATIVE && isFlying) || gamemode == Gamemode.SPECTATOR) {
                     var pos = rigidBody.position;
                     pos.y += 0.1f;
                     rigidBody.Move(pos, Quaternion.identity);
-                } else {
-                    isGrounded = false;
-                    rigidBody.AddForce(Vector3.up * (jumpForce * 100));
                 }
             } else if (Input.GetKey(KeyCode.LeftShift)) {
                 if (gamemode == Gamemode.SPECTATOR || (gamemode == Gamemode.CREATIVE && isFlying)) {
@@ -97,7 +118,7 @@ namespace World.Entity {
 
         private void LateUpdate() {
             if (IsMenuOpen()) {
-                rigidBody.velocity = new Vector3(0, 0, 0);
+                ResetVelocity();
                 return;
             }
 
@@ -106,13 +127,14 @@ namespace World.Entity {
 
             // Rotate camera vertical
             var rotY = Mathf.Clamp(_mouseVertical * 5.0f, -90f, 90f);
-            camera.transform.localRotation = Quaternion.Euler(-rotY, 0f, 0f);
+            playerCamera.transform.localRotation = Quaternion.Euler(-rotY, 0f, 0f);
 
             // Move player
             var speed = isSprinting ? sprintSpeed : walkSpeed;
+            if (isFlying) speed *= 1.5f;
             var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed;
             vel.y = rigidBody.velocity.y;
-            // If the player stops, stop their sprinting
+            // If the player stops moving, stop their sprinting
             if (vel.z <= walkSpeed) {
                 isSprinting = false;
             }
@@ -123,6 +145,10 @@ namespace World.Entity {
 
         private bool IsMenuOpen() {
             return menuScreen.activeSelf;
+        }
+
+        private void ResetVelocity() {
+            rigidBody.velocity = new Vector3(0, 0, 0);
         }
 
         private static void LockCursor(bool locked) {
@@ -142,7 +168,7 @@ namespace World.Entity {
             if (highlightBlock.gameObject.activeSelf) {
                 if (Input.GetMouseButtonDown(0)) {
                     world.SetBlock(highlightBlock.position.ToVector3Int(), Blocks.AIR);
-                } else if (Input.GetMouseButtonDown(1)) {
+                } else if (Input.GetMouseButtonDown(1) && placeBlock.gameObject.activeSelf) {
                     world.SetBlock(placeBlock.position.ToVector3Int(), Blocks.DIRT);
                 }
             }
@@ -153,21 +179,24 @@ namespace World.Entity {
             var lastPos = new Vector3();
 
             while (step < reach) {
-                var transform1 = camera.transform;
+                var transform1 = playerCamera.transform;
                 var pos = transform1.position + (transform1.forward * step);
 
                 if (world.CheckForBlock(pos.ToVector3Int())) {
-                    highlightBlock.position = new Vector3(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y),
-                        Mathf.FloorToInt(pos.z));
+                    highlightBlock.position = pos.ToVector3Int();
                     placeBlock.position = lastPos;
 
                     highlightBlock.gameObject.SetActive(true);
-                    placeBlock.gameObject.SetActive(true);
+                    if (transform.position.ToVector3Int() != placeBlock.position.ToVector3Int()) {
+                        placeBlock.gameObject.SetActive(true);
+                    } else {
+                        placeBlock.gameObject.SetActive(false);
+                    }
 
                     return;
                 }
 
-                lastPos = new Vector3(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), Mathf.FloorToInt(pos.z));
+                lastPos = pos.ToVector3Int();
                 step += checkIncrement;
             }
 
